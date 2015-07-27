1b0f5975777093e267f2d45d3bf5149d1bed5155
Wei Wang <chengtian.wang@gmail.com>
2015-07-26 21:55:28 -0700

    Remove duplicated build errors

--- src/Microsoft.Framework.PackageManager/Building/BuildContext.cs
+++ src/Microsoft.Framework.PackageManager/Building/BuildContext.cs
@@ -66,9 +66,6 @@ namespace Microsoft.Framework.PackageManager
                 diagnostics.AddRange(result.Diagnostics);
             }
 
-            var errors = _applicationHostContext.DependencyWalker.GetDependencyDiagnostics(_project.ProjectFilePath);
-            diagnostics.AddRange(errors);
-
             return result.Success && !diagnostics.HasErrors();
         }
 
